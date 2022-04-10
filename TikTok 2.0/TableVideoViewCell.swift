bd4fdf22f31925a2572a4a1e6217ac91a8b12257
OlegKotkov <tigrastov@gmail.com>
2022-04-10 19:41:44 +0200

    init commit Study TikTok

---
+++ TikTok 2.0/TableVideoViewCell.swift
@@ -0,0 +1,40 @@
+
+import BMPlayer
+import SnapKit
+import UIKit
+
+
+class TableVideoViewCell: UITableViewCell {
+    
+    private var links: String
+    private var player = BMPlayer()
+    
+
+    init(links: String) {
+        self.links = links
+        super.init(style: .default, reuseIdentifier: "cell")
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func initialize () {
+        player.isUserInteractionEnabled = false
+        BMPlayerConf.enableVolumeGestures = false
+        BMPlayerConf.enableBrightnessGestures = false
+        BMPlayerConf.enablePlaytimeGestures = false
+        BMPlayerConf.shouldAutoPlay = true
+        
+        
+       let resource = BMPlayerResource(url: URL(string: links)!)
+        player.setVideo(resource: resource)
+        self.contentView.addSubview(player)
+        player.snp.makeConstraints { maker in
+            maker.edges.equalToSuperview()
+        }
+    }
+    
+    
+    
+}
